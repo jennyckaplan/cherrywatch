654c6b59b79375dfbdb35f07f3e60ebbe9365760
Jen Kaplan <jenniferckaplan@gmail.com>
2022-04-15 12:22:53 -0400

    use env variables, set up snapshot updates, add in the cherry stages to snapshot

--- cherrywatch.py
+++ cherrywatch.py
@@ -1,30 +1,36 @@
 import requests
+import os
+import sys
 from enum import Enum
+from datetime import date
 import re
 import json
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 
-account_sid = "<account_sid>"
-auth_token = "<auth_token>"
+ACCOUNT_SID = os.environ.get("ACCOUNT_SID")
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
+TWILIO_NUMBER = os.environ.get("TWILIO_NUMBER")
+RECIPIENT = os.environ.get("RECIPIENT")
+
+account_sid = ACCOUNT_SID
+auth_token = AUTH_TOKEN
 
 client = Client(account_sid, auth_token)
 
+
 class BloomStage(Enum):
-  prebloom = "Prebloom"
-  first_bloom = "First Bloom"
-  peak_bloom = "Peak Bloom"
-  post_peak_bloom = "Post-Peak Bloom"
+    prebloom = "Prebloom"
+    first_bloom = "First Bloom"
+    peak_bloom = "Peak Bloom"
+    post_peak_bloom = "Post-Peak Bloom"
+
 
 URL = "https://www.bbg.org/collections/cherries"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.text, "html.parser")
 raw_html = soup.prettify()
-
-# TODO: save the raw_html for each day
-
-# TODO: save the image of map
 
 script_with_tree_data = soup.find_all("script")[1].string.strip()
 
@@ -39,35 +45,47 @@ prunuses = re.sub(",[ \t\r\n]+\]", "]", prunuses)
 prunuses = json.loads(prunuses)
 
 bloom_stage_count = {
-  BloomStage.prebloom.value: 0,
-  BloomStage.first_bloom.value: 0,
-  BloomStage.peak_bloom.value: 0,
-  BloomStage.post_peak_bloom.value: 0,
+    BloomStage.prebloom.value: 0,
+    BloomStage.first_bloom.value: 0,
+    BloomStage.peak_bloom.value: 0,
+    BloomStage.post_peak_bloom.value: 0,
 }
 
 update_message = ""
 tree_count = len(prunuses)
-update_message += f"There are {tree_count} cherry blossom trees at the Brooklyn Botanic Garden\n\n"
+update_message += "Welcome to Jen's Cherry Blossom Watch! \n\n"
+update_message += (
+    f"Out of the {tree_count} cherry blossom trees at the Brooklyn Botanic Garden,\n\n"
+)
 update_message += "Today,\n\n"
 
 for tree in prunuses:
     bloom_stage = tree[7]
     bloom_stage_count[bloom_stage] += 1
 
-# TODO: save bloom stage count for each day
-    
+
 def get_percentage(stage: BloomStage) -> float:
     return round(bloom_stage_count[stage.value] / tree_count, 4)
+
 
 def get_formatted_percentage(stage: BloomStage) -> str:
     percentage = get_percentage(stage)
     return f"{percentage * 100}%"
 
+
 for stage in BloomStage:
     percentage = get_formatted_percentage(stage)
     update_message += f"{percentage} are at {stage.value}\n"
-    
-message = client.messages.create(
-  to="<phone_number>", 
-  from_="<twilio_phone_number>", 
-  body=update_message)
+
+today = date.today()
+today = today.strftime("%-m-%-d-%Y")
+
+try:
+    message = client.messages.create(
+        to=RECIPIENT,
+        from_=TWILIO_NUMBER,
+        body=update_message,
+        media_url=f"https://raw.githubusercontent.com/jennyckaplan/cherrywatch/main/images/{today}.png",
+    )
+except:
+    sys.exit(1)
--- cherrywatch.test.js
+++ cherrywatch.test.js
@@ -14,15 +14,30 @@ describe("visual snapshots", () => {
 
   it('get visual snapshot of cherry watch map', async () => {
     page = await browser.newPage();
+
     await page.goto(url);
-    await page.waitForSelector("#cherrymap");
-    const map = await page.$("#cherrymap");
+
+    await page.waitForSelector("#map");
+
+    // remove the DOM nodes we don't want in the screenshot
+    await page.evaluate(() => {
+      const mapContainer = document.querySelector("#map");
+
+      const elementsToRemove = document.querySelectorAll("#map > :not(.cherrystages, #cherrymap)");
+
+      elementsToRemove.forEach((element) => {
+        mapContainer.removeChild(element);
+      });
+    });
+
+    const map = await page.$("#map");
+
     const today = new Date().toLocaleDateString().replace(/\//g, '-');
     const image = await map.screenshot({
       path: `./images/${today}.png`,
     });
 
-    // TODO: highlight the diff each day + save
+    // TODO: if diff, send the text
     expect(image).toMatchImageSnapshot();
   });
 
